aefd45a4e1b910b43afcbcca32dad5cc09688f6a
Riley Hardy <54298858+rileyjhardy@users.noreply.github.com>
2021-08-27 16:22:06 -0500

    add create and destroy actions for session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = User.articles.paginate(page: params[:page], per_page: 5)
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
