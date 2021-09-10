23cc4ad5e26eb76a463e1be68900e1412fd5d23a
Riley Hardy <54298858+rileyjhardy@users.noreply.github.com>
2021-09-10 15:15:08 -0500

    update user permissions and other stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show,:edit,:update,:destroy]
-
+    before_action :require_user, except: [:show, :index ]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     def show        
         
     end
@@ -13,9 +14,7 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
-    def edit
-        
-        
+    def edit      
 
     end
 
@@ -43,8 +42,7 @@ class ArticlesController < ApplicationController
         end
     end
 
-    def destroy
-        
+    def destroy        
         @article.destroy
         redirect_to articles_path
     end
@@ -57,5 +55,12 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+        if current_user != @article.user
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to user_path
+        end
     end
 end
